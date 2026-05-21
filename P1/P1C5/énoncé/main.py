2c0a9761403850ce9fa774779ef49cdafc2b179d
mohamedsoare910-maker <mohamedsoare910@gmail.com>
2026-05-21 15:57:34 +0200

    Update main.py exercice python

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,13 @@
-# Écrivez votre code ici !
+nom= "Mohamed"
+age= 20
+taille= 1,72
+est etudiant= true
+print(nom)
+print(age)
+print(taille)
+print(est etudiant)
+
+print(type(nom))
+print(type(age))
+print(type(taille))
+print(type(est etudiant))
